660a8866119baf2bb20fb1308f5576ec50913f61
MECCNR <e.migalckin@yandex.ru>
2022-07-17 19:05:16 +0900

    v2

--- src/Main.java
+++ src/Main.java
@@ -3,22 +3,22 @@ public class Main {
         int money = 100;
         // начальное кол-во денег
 
-        int add_money = 2000;
+        int addMoney = 2000;
         // введённое кол-во денег на счёт
 
-        int bonus = add_money / 100;
+        int bonus = addMoney / 100;
         // бонус по акции
 
-        int all_money = money + add_money;
+        int all_money = money + addMoney;
         // общее кол-во денег при пополнении счёта
 
-        if (add_money > 1000) {
+        if (addMoney > 1000) {
             all_money = all_money + bonus;
         }
         // общее кол-во денег при пополнении счёта вместе с бонусом акции (при учёте того, если условие выполнено)
 
-        System.out.println("Вы пополнили счёт на " + (add_money) + " рублей");
-        if (add_money > 1000) {
+        System.out.println("Вы пополнили счёт на " + (addMoney) + " рублей");
+        if (addMoney > 1000) {
             System.out.println("Вы активировали акцию \"Получи рубль за каждые 100!\". Ваш бонус составляет: " + bonus);
         }
         System.out.println("Итоговый счёт: " + all_money);
